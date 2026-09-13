bb3eb1dc1a359fd874fafec1507351e6636aee41
Karthikeya Thumma <129876827+thummakarthikeya@users.noreply.github.com>
2026-09-13 22:06:28 +0530

    Add middleware explanation to Express.js interview questions
    Added explanation of middleware in Express.js.

--- JavaScript/Express.js Interview Questions (API Framework)/Express.js
+++ JavaScript/Express.js Interview Questions (API Framework)/Express.js
@@ -1,4 +1,11 @@
 Express.js Interview Questions (API Framework)
 
 1. What is middleware in Express?
+  -->Middleware is a function that runs between the request coming in and the response going out. It’s commonly used for tasks like
+      authentication, logging, request validation, and error handling.
+
+
+
+        
+2. How does routing work in Express (Router, params, query)?
   -->
